2227f02dda7f6cfb469fdd8ae090219cef018fd5
nhahn <Nathan.P.Hahn@boeing.com>
2012-04-08 01:47:23 -0400

    Simple changes to layout and proper root configuration

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,23 +8,31 @@ class Ability
     if user.role? :admin
       can :manage, :all
     elsif user.role? :manager
-	  can :update, Store do |store|
-	    store.id == current_user.assignment.store_id
-	  end
-	  can :manage, Shift do |shift|
-	    shift.assignment.store_id == current_user.assignment.store_id
-	  end
-	  can :create, Job
-	  can :read, Job
-	  can :update, Job
-
+      can :update, Store do |store|
+        store.id == current_user.employee.assignment.store_id
+      end
+      can :manage, Shift do |shift|
+        shift.assignment.store_id == current_user.employee.assignment.store_id
+      end
+      can :read, Employee do |employee|
+        employee.assignment.store_id = current_user.employee.assingment.store_id
+      end
+      can :update, Employee do |employee|
+        employee.assignment.store_id = current_user.employee.assingment.store_id
+      end
+      can :create, Job 
+      can :read, Job
+      can :update, Job
     elsif user.role? :employee
-	  can :update, User do |user|
-	    user.id == current_user.id
-	  end
-	else
-	  can :read, Store
-	  can :create, User
+      can :update, User do |user|
+        user.id == current_user.id
+      end
+      can :read, Employee do |employee|
+        employee.id == current_user.employee_id
+      end
+    else
+      can :read, Store
+      can :create, User
     end
   
   end
