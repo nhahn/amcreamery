f35b8edb1c8158de849ca77b27a6ecfa81173b99
nhahn <Nathan.P.Hahn@boeing.com>
2012-05-03 11:42:22 -0400

    Refactored some code for user roles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -26,13 +26,13 @@ class HomeController < ApplicationController
         end
         @date = @date - (@date.wday==0 ? 6 : @date.wday-1).days
         @start_date = Date.new(@date.year, @date.month, @date.day)
-      if  current_user.employee.role == "admin"
+      if  current_user.role? :admin
         @sortable = SortIndex::Sortable.new(params, INDEX_SORT)
         @stores = Store.paginate(:page => params[:page]).order(@sortable.order).per_page(10)
         @hours = Employee.all.collect{|emp| {:emp => emp, :hours => emp.shifts.inject(0){|sum, n| (n.date >= Date.parse(2.week.ago.to_s))? sum + n.hours : sum += 0}}}.sort{|a,b| [b[:hours], a[:emp].last_name] <=> [a[:hours], b[:emp].last_name]}.first(5)
         @events = Shift.where('date between ? and ?', @start_date, @start_date+7).chronological.to_a        
 
-      elsif current_user.employee.role == "manager"
+      elsif current_user.role? :manager
 
         store = current_user.employee.current_assignment.store
         @sortable = SortIndex::Sortable.new(params, EMPLOYEE_SORT)
@@ -46,7 +46,7 @@ class HomeController < ApplicationController
         @events = Shift.joins(:assignment).where('end_date is NULL').where('store_id = ?', store.id).where('date > ? and date <= ?', @start_date, @start_date+7).chronological.to_a
         @incomplete = Shift.past.chronological.map{|shift| shift if shift.shift_jobs.empty?}.compact.first(9)
       
-      elsif current_user.employee.role == "employee"
+      elsif current_user.role? :employee
 
         @employee = current_user.employee
         @events = Shift.joins(:assignment).where('employee_id = ?', current_user.employee.id).where('date > ? and date <= ?', @start_date, @start_date+7).chronological.to_a
